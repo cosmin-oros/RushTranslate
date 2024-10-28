f1cf2560c1f65f2792bc25613d3d36dcbc08efcc
Liviuu1 <liviu.oprea@student.upt.ro>
2024-10-28 21:25:55 +0200

    screens setup

---
+++ client/screens/package_details/PackageDetails.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Container, Content } from '../../tamagui.config';
+import { useTranslation } from 'react-i18next';
+
+const PackageDetailsScreen: React.FC = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Container>
+      {/* Header */}
+
+      <Content>
+        
+      </Content>
+
+    </Container>
+  );
+};
+
+export default PackageDetailsScreen;
