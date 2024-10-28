89a340d60e6193f7ae024aaa866849ba0eb40b4f
Liviuu1 <liviu.oprea@student.upt.ro>
2024-10-28 21:31:10 +0200

    routes

--- client/App.tsx
+++ client/App.tsx
@@ -6,6 +6,10 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/home/HomeScreen';
 import { Routes } from './routes/routes';
+import WelcomeScreen from './screens/welcome/WelcomeScreen';
+import SettingsScreen from './screens/settings/SettingsScreen';
+import PackageDetailsScreen from './screens/package_details/PackageDetailsScreen';
+import SavedScreen from './screens/saved/SavedScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,7 +20,10 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator initialRouteName={Routes.Home}>
           <Stack.Screen name={Routes.Home} component={HomeScreen} options={{ headerShown: false }} />
-                  
+          <Stack.Screen name={Routes.Welcome} component={WelcomeScreen} options={{ headerShown: false }} />
+          <Stack.Screen name={Routes.Settings} component={SettingsScreen} options={{ headerShown: false }} />
+          <Stack.Screen name={Routes.PackageDetails} component={PackageDetailsScreen} options={{ headerShown: false }} />
+          <Stack.Screen name={Routes.Saved} component={SavedScreen} options={{ headerShown: false }} />
         </Stack.Navigator>
       </NavigationContainer>
     </TamaguiProvider>
