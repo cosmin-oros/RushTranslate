268eed1f1d2d16d22260f67232bbe270f5544b73
cosmin.oros <oroscosmin09@gmail.com>
2024-10-31 16:09:59 +0200

    welcome screen initial design

--- client/App.tsx
+++ client/App.tsx
@@ -1,24 +1,48 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { TamaguiProvider } from 'tamagui';
 import tamaguiConfig from './tamagui.config';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/home/HomeScreen';
-import { Routes } from './routes/routes';
 import WelcomeScreen from './screens/welcome/WelcomeScreen';
 import SettingsScreen from './screens/settings/SettingsScreen';
 import PackageDetailsScreen from './screens/package_details/PackageDetailsScreen';
 import SavedScreen from './screens/saved/SavedScreen';
+import { Routes } from './routes/routes';
+import * as SplashScreen from 'expo-splash-screen';
+import i18n from './i18n';
+import 'intl-pluralrules';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [isAppReady, setAppReady] = useState(false);
+
+  useEffect(() => {
+    const prepareApp = async () => {
+      try {
+        await i18n.init();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppReady(true);
+        await SplashScreen.hideAsync();
+      }
+    };
+
+    prepareApp();
+  }, []);
+
+  if (!isAppReady) {
+    return null; 
+  }
 
   return (
     <TamaguiProvider config={tamaguiConfig}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName={Routes.Home}>
+        <StatusBar style="auto" />
+        <Stack.Navigator initialRouteName={Routes.Welcome}>
           <Stack.Screen name={Routes.Home} component={HomeScreen} options={{ headerShown: false }} />
           <Stack.Screen name={Routes.Welcome} component={WelcomeScreen} options={{ headerShown: false }} />
           <Stack.Screen name={Routes.Settings} component={SettingsScreen} options={{ headerShown: false }} />
@@ -29,4 +53,3 @@ export default function App() {
     </TamaguiProvider>
   );
 }
-
