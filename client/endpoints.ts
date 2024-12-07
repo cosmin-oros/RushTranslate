65f5620a25b8143289fb0f83af885c0d30cd38c7
cosmin.oros <oroscosmin09@gmail.com>
2024-12-07 12:51:17 +0200

    fix to env base url

--- client/endpoints.ts
+++ client/endpoints.ts
@@ -1,11 +1,11 @@
-import { API_BASE_URL } from "@env";
+import { EXPO_PUBLIC_API_BASE_URL } from "@env";
 
 export const ENDPOINTS = {
-  getPackages: `${API_BASE_URL}/packages`, // List all available translation packages
+  getPackages: `${EXPO_PUBLIC_API_BASE_URL}/packages`, // List all available translation packages
   getPackageTranslations: (packageName: string) =>
-    `${API_BASE_URL}/${packageName}`, // Get all translations for a specific package
+    `${EXPO_PUBLIC_API_BASE_URL}/${packageName}`, // Get all translations for a specific package
   getLanguageTranslations: (packageName: string, language: string) =>
-    `${API_BASE_URL}/${packageName}/${language}`, // Get translations for a specific language in a package
+    `${EXPO_PUBLIC_API_BASE_URL}/${packageName}/${language}`, // Get translations for a specific language in a package
   addOrUpdateTranslations: (packageName: string) =>
-    `${API_BASE_URL}/${packageName}`, // Add or update translations for a specific language in a package
+    `${EXPO_PUBLIC_API_BASE_URL}/${packageName}`, // Add or update translations for a specific language in a package
 };
